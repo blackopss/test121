689725528f8b4a56910ace20b3d5dbab1ccd1c7d
blackopss <36933564+blackopss@users.noreply.github.com>
2018-08-07 16:27:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 const fs = require('fs');
 const jimp = require('jimp')
+const moment = require('moment')
 const canvas = require('canvas')
 const prefix = "e-";
 const adminprefix = "e--";
